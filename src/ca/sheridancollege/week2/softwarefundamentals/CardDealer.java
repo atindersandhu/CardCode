7be550191e3fd24b5704056ec01365492fdf1101
atindersandhu <sin11288@sheridancollege.ca>
2022-02-02 15:06:57 -0500

    created object for card class
    created object for card class

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -15,5 +15,7 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        
+        Card atinderPalSingh = new Card("Spade", 7);
     }
 }
